d1433d463483ecd014f7d3825e2f9b3328cb1d89
kitosina <kitosinaa555@mail.ru>
2020-09-21 18:09:52 +0700

    init commit

---
+++ src/test/java/ru/sibsutis/kitosina/TestExample/TestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sibsutis.kitosina.TestExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
